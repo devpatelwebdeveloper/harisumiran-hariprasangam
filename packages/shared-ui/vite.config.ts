2e8b9b1df56d6503b4b9b2154e6dab0f65dfdb39
Devashish Patel <dev.patel8787@gmail.com>
2023-03-14 22:34:55 -0400

    component repo setup complete

--- packages/shared-ui/vite.config.ts
+++ packages/shared-ui/vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dts from "vite-plugin-dts"
 import {resolve} from 'pathe'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+
+      insertTypesEntry:true,
+    }
+  )
+],
   build:{
     lib:{
       // Could also be a dictionary or array of multiple entry points
