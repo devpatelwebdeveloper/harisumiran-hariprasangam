2ba6f21ffc95e22a9361a4155a2780e5c4ac41c7
Devashish Patel <dev.patel8787@gmail.com>
2023-03-14 21:41:06 -0400

    initial setup

--- packages/shared-ui/vite.config.ts
+++ packages/shared-ui/vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import {resolve} from 'pathe'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build:{
+    lib:{
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'SharedUi',
+      // the proper extensions will be added
+      fileName: 'shared-ui',
+      formats:['es','cjs','umd','iife']
+    },
+  }
 })
