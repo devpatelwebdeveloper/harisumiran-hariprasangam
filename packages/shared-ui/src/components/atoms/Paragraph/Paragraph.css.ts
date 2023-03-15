3a85d61394edf0ca7d0e27dad74def6f0410926a
Devashish Patel <dev.patel8787@gmail.com>
2023-03-15 13:23:26 -0400

    vanilla extract setup test

--- packages/shared-ui/src/components/atoms/Paragraph/Paragraph.css.ts
+++ packages/shared-ui/src/components/atoms/Paragraph/Paragraph.css.ts
@@ -1,6 +1,6 @@
 import { styleVariants } from '@vanilla-extract/css';
 
 export const textColor = styleVariants({
-  primary: { color: 'blue' },
-  secondary: { color: 'aqua' }
+  primary: { color: 'green' },
+  secondary: { color: 'red' }
 });
